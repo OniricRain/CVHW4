6d99158fef049374069f03a435af884783cabbc7
Luigi <elledelucia@gmail.com>
2020-06-15 10:55:57 +0200

    final version

--- HW4/HW4.cpp
+++ HW4/HW4.cpp
@@ -11,52 +11,49 @@ using namespace cv;
 
 
 // starting point for the application
-int main(int argc, char** argv)//TODO control this
+int main()
 {
-	vector<cv::String> filepath;
-	// load an image and place it in the img variable
-	//glob("lab/*.bmp",filepath, false);
-	glob("kitchen/*.bmp", filepath, false);
-
-	Mat imgleft = cv::imread(filepath[0]);
-	int firstWidth = imgleft.cols;
-	imgleft = cylindricalProj(imgleft, 33);
-	//imshow("testcolor", imgleft);
-	imgleft = imageEqualization((void*)&imgleft);
-
-
-	//TODO try CLAHE
-	int n = filepath.size();
-	//int n = 3;
-	Mat result = imgleft;
-	for (size_t i = 1; i < n; i++)
-	{
-		std::cout << "Processing image number " << i << endl;
-		Mat imgright = cv::imread(filepath[i]);
-		imgright = imageEqualization((void*)&imgright);
-		addRight(imgleft, imgright, result,firstWidth,i);
-		imgleft = imgright;
-	}
-	//cv::imshow("result", result);
-	imwrite("resultBlended.png", result);
+	int desc, set;
+	selectDescSet(desc,set);
 	
+	vector<cv::String> filepath;
+	Mat imgleft;
+	vector<double> shifts;
+	double yshift, oshiftPlus,oshiftMinus;
+	for (size_t num_matches =800; num_matches <= 800; num_matches += 10)
+	{
+		oshiftPlus = 0;
+		oshiftMinus = 0;
+		acquisition(filepath, imgleft,set);
+			   		
+		int n = filepath.size();
+		//int n = 14;
+		Mat result = imgleft;
+		for (size_t i = 1; i < n; i++)
+		{
+			yshift = 0;
+			std::cout << "Processing image number " << i << " NUM MATCHES " << num_matches << endl;
+			Mat imgright = cv::imread(filepath[i]);
+			addRight(imgleft, imgright, result, i, num_matches, yshift, desc, set);
+			updateShift(yshift, oshiftPlus, oshiftMinus);
+			imgleft = imgright;
+		}
+		shifts.push_back(std::max(std::abs(oshiftMinus), oshiftPlus));
+		lastCut(result, oshiftPlus, oshiftMinus);// TODO uncomment
+	}
 
-	/*
-	//-- Draw keypoints
-	Mat img_keypoints;
-	drawKeypoints(res, keypoints1, img_keypoints);
-	//-- Show detected (drawn) keypoints
-	imshow("ORB Keypoints", img_keypoints);
-	*/
 
-	/*
-	Mat img_out = imageEqualization((void*)&img);
-	//show the final equalized image
-	namedWindow("window_2", 1);
-	cv::imshow("window_2", img_out);*/
 	
 
 	// wait for a key to be pressed and then close all
+	cout << "all the shifts:" << endl;
+	int num_matches = 150;
+	for (std::vector<double>::const_iterator i = shifts.begin(); i != shifts.end(); ++i) {
+		std::cout << "num " << num_matches << " " << *i << endl;
+		num_matches += 10;
+	}
+	
+
 	cv::waitKey(0);
 	cv::destroyAllWindows();
 
--- HW4/myinclude.h
+++ HW4/myinclude.h
@@ -11,15 +11,38 @@
 using namespace std;
 using namespace cv;
 
+/**
+ * @brief  Selection between two descriptors
+ * 
+ * @return int the selected descriptor
+ */
+void selectDescSet(int& desc, int& set) {
+	
+	do {
+		cout << "select descriptor, type a number:" << endl;
+		cout << "1) ORB" << endl << "2) AKAZE" << endl;
+		cin >> desc;
+		//desc = 1; //TODO remove
+	} while (desc != 1 && desc != 2);
+	do {
+		cout << "select imageset, type a number:" << endl;
+		cout << "1) Lab" << endl << "2) Kitchen" << endl << "3) Dolomites" << endl << "4) Fountain" << endl;
+		cin >> set;
+		//set = 4;//TODO remmove
+	} while (set != 1 && set != 2 && set != 3 && set != 4) ;
+}
 
+/**
+ * @brief performs histogram equalization on an input image,
+ * the equalization is done in the LAB colorspace, only on the L channel
+ * 
+ * @param userdata the input image
+ * @return cv::Mat the equalized image
+ */
 cv::Mat imageEqualization(void* userdata)
 {
 	cv::Mat img = *(cv::Mat*)userdata;
-	/*
-	//Create a window
-	namedWindow("window_1", 1);
-	cv::imshow("window_1", img);
-	*/
+
 	//Converting image in LAB color space
 	cvtColor(img, img, COLOR_BGR2Lab);
 
@@ -52,21 +75,19 @@ cv::Mat imageEqualization(void* userdata)
 	//Converting back to BGR colorspace
 	cvtColor(img_out, img_out, COLOR_Lab2BGR);
 
-	//show the final equalized image
-
-
-
-	// wait for a key to be pressed and then close all
-
-
 	return img_out;
 }
 
+/**
+ * @brief Perform a cylindrical projection on a given input image
+ * the projection is done on each RGB channel, then the channels are merged
+ * 
+ * @param image input image
+ * @param angle is the camera Field of View divided by 2
+ * @return cv::Mat output image
+ */
 cv::Mat cylindricalProj(const cv::Mat& image, const double angle)
 {
-	//cv::Mat tmp, result;
-	/*cv::cvtColor(image, tmp, cv::COLOR_BGR2GRAY);
-	result = tmp.clone();*/
 	cv::Mat channels[3], result[3];
 	Mat resultMerged;
 	split(image, channels);
@@ -95,7 +116,7 @@ cv::Mat cylindricalProj(const cv::Mat& image, const double angle)
 				y1 < half_height_image &&
 				y1 > -half_height_image + 1)
 			{
-				//modified for color compatibility
+				//modified for color compatibility, perform the projection on each channel
 				result[0].at<uchar>(y + half_height_image, x + half_width_image)
 					= channels[0].at<uchar>(round(y1 + half_height_image),
 						round(x1 + half_width_image));
@@ -105,7 +126,7 @@ cv::Mat cylindricalProj(const cv::Mat& image, const double angle)
 				result[2].at<uchar>(y + half_height_image, x + half_width_image)
 					= channels[2].at<uchar>(round(y1 + half_height_image),
 						round(x1 + half_width_image));
-				merge(result, 3, resultMerged);
+				merge(result, 3, resultMerged); // merging the three projected channels
 			}
 		}
 	}
@@ -113,6 +134,65 @@ cv::Mat cylindricalProj(const cv::Mat& image, const double angle)
 	return resultMerged;
 }
 
+/**
+ * @brief program inizialization, selection of the imageset and
+ * preprocessing (cylindrical projection and histogram equalization)
+ * of the first image
+ * 
+ * @param filepath path of the selected imageset
+ * @param imgleft first image of the panoramic
+ */
+void acquisition(vector<cv::String>& filepath, Mat& imgleft, int& set) {
+	switch (set)
+	{
+		case 1://lab set
+		{
+			glob("lab/*.bmp", filepath, false);
+			//glob("pL/*.bmp", filepath, false);
+			break;
+		}
+		case 2://kitchen set
+		{
+			//glob("pK/*.bmp", filepath, false);
+			glob("kitchen/*.bmp", filepath, false);
+			break;
+		}
+		case 3://dolomites set
+		{
+			glob("dolomites/*.png", filepath, false);
+			break;
+		}
+		case 4://fountain set
+		{
+			glob("fountain/*.jpg", filepath, false);
+			//glob("pM/*.jpg", filepath, false);
+			break;
+		}
+	default:
+		break;
+	}
+
+
+
+	imgleft = cv::imread(filepath[0]);
+	imgleft = cylindricalProj(imgleft, 33);// TODO UNCOMMENT//51/2 sony
+	imgleft = imageEqualization((void*)&imgleft);// TODO UNCOMMENT
+	
+	//string path = "pM/00.jpg";
+	//imwrite(path, imgleft);
+}
+
+/**
+ * @brief creating a mask which allows to select only 2/3 of the image
+ * the descriptors will run only in this region of interest for each image
+ * left image mask -> 2/3 on the right side
+ * right image mask -> 2/3 on the left side
+ *  
+ * @param imgleft left image
+ * @param imgright right image
+ * @param maskRight ROI of left image
+ * @param maskLeft ROI of right image
+ */
 void maskImages(const Mat& imgleft, const Mat& imgright, Mat& maskRight, Mat& maskLeft) {
 	//create mask for search keypoints in only half image
 	maskRight = Mat::zeros(imgleft.size(), CV_8U);
@@ -121,23 +201,113 @@ void maskImages(const Mat& imgleft, const Mat& imgright, Mat& maskRight, Mat& ma
 	maskLeft = Mat::zeros(imgright.size(), CV_8U);
 	Mat left(maskLeft, Rect(0, 0, imgright.cols * 2/3, imgright.rows));
 	left = Scalar(255);
+	imwrite("right.png", maskLeft);
+	imwrite("left.png", maskRight);
 }
 
-void orbDetection(Mat& img, Mat& mask, vector<KeyPoint>& keypoints, Mat& descriptors) {
-	int nfeatures = 500;//300
-	int FASTthres = 20;//25
+/**
+ * @brief initialize the ORB descriptor with parameters and perform feature extraction and descritption
+ * 
+ * @param img input image
+ * @param mask relative mask computed with maskImages
+ * @param keypoints output keypoints
+ * @param descriptors output descriptors
+ * @param num_matches for testing purposes, set the number of features to detect
+ */
+void orbDetComp(Mat& img, Mat& mask, vector<KeyPoint>& keypoints, Mat& descriptors,int num_matches, int& set,int i) {
+	int nfeatures;
+	switch (set)
+	{
+	case 1://lab set
+	{
+		nfeatures = 340;//300
+		break;
+	}
+	case 2://kitchen set
+	{
+		nfeatures = 500;
+		break;
+	}
+	case 3://dolomites set
+	{
+		nfeatures = 500;
+		break;
+	}
+	case 4://fountain set
+	{
+		nfeatures = 410;
+		break;
+	}
+	default:
+		break;
+	}
+	int FASTthres = 20;
 	Ptr<ORB> orb = ORB::create(nfeatures,1.2f,8,31,0,2,ORB::HARRIS_SCORE,31,FASTthres);
-	//Ptr<AKAZE> akaze = AKAZE::create(); TODO IMPLEMENT AKAZE
 	orb->detectAndCompute(img, mask, keypoints, descriptors);
-	//akaze->detectAndCompute(img, mask, keypoints, descriptors);
 }
 
+
+/**
+ * @brief initialize the AKAZE descriptor with paramers and perform feature extraction and descritption
+ * 
+ * @param img input image
+ * @param mask relative mask computed with maskImages
+ * @param keypoints output keypoints
+ * @param descriptors output descriptors
+ * @param num_matches //TODO remove
+ */
+void akazeDetComp(Mat& img, Mat& mask, vector<KeyPoint>& keypoints, Mat& descriptors, int num_matches, int& set) {
+	float thres;
+	switch (set)
+	{
+	case 1://lab set
+	{
+		thres = 1/float(480);
+		break;
+	}
+	case 2://kitchen set
+	{
+		thres = 1 / float(320);
+		break;
+	}	 
+	case 3://dolomites set
+	{	 
+		thres = 0.001;
+		break;
+	}
+	case 4://fountain set
+	{
+		thres = 1 / float(460);
+		break;
+	}
+	default:
+		break;
+	}
+	Ptr<AKAZE> akaze = AKAZE::create(AKAZE::DESCRIPTOR_MLDB,0,3,thres,4,4,KAZE::DIFF_PM_G2);
+	akaze->detectAndCompute(img, mask, keypoints, descriptors);
+}
+
+/**
+ * @brief Brute-Force feature matching
+ * 
+ * @param descriptors1 descriptors computed for left image
+ * @param descriptors2 descriptors computed for right image
+ * @param matches output vector of matches
+ */
 void matchingBF(Mat& descriptors1, Mat& descriptors2, vector<DMatch>& matches) {
 	Ptr<cv::DescriptorMatcher> matcher(new cv::BFMatcher(cv::NORM_HAMMING, true));//create matcher
 	matcher->match(descriptors1, descriptors2, matches);
 }
-	
-void matchingRefine(vector<DMatch>& matches, vector<KeyPoint>& keypoints1, vector<KeyPoint>& keypoints2, vector<Point2f>& kp1_refined, vector<Point2f>& kp2_refined ){
+
+/**
+ * @brief experimental procedure to filter out the matches with minimum hamming distance,
+ * the objective was to use RANSAC only on these matches.
+ * Since this procedure didn't improve results, this function is no longer used
+ * 
+ * @param matches matches founded by matchingBF
+ * @return vector<DMatch> output matches with minimum distance
+ */
+vector<DMatch> minHamming(vector<DMatch>& matches) {
 	Mat index;
 	int nbMatch = int(matches.size());
 	Mat tab(nbMatch, 1, CV_32F);
@@ -145,50 +315,48 @@ void matchingRefine(vector<DMatch>& matches, vector<KeyPoint>& keypoints1, vecto
 		tab.at<float>(i, 0) = matches[i].distance;
 	sortIdx(tab, index, SORT_EVERY_COLUMN + SORT_ASCENDING);
 	vector<DMatch> bestMatches;
-	int limit = 150;//GOOD
-		if (nbMatch < limit) {
-			limit = nbMatch;
-		}
+	int limit = 100;
+	if (nbMatch < limit) {
+		limit = nbMatch;
+	}
 	for (int i = 0; i < limit; i++)
 		bestMatches.push_back(matches[index.at < int >(i, 0)]);
-	
+	return bestMatches;
+}
 
-	for (vector<DMatch>::iterator it = bestMatches.begin(); it != bestMatches.end(); ++it) {
+/**
+ * @brief filter the keypoints that have been matched
+ * 
+ * @param matches matches founded by matchingBF
+ * @param keypoints1 keypoints computed for left image
+ * @param keypoints2 keypoints computed for right image
+ * @param kp1_refined output keypoints for left image 
+ * @param kp2_refined output keypoints for right image
+ * @param num_matches //TODO remove
+ */
+void matchingRefine(vector<DMatch>& matches, vector<KeyPoint>& keypoints1, vector<KeyPoint>& keypoints2, vector<Point2f>& kp1_refined, vector<Point2f>& kp2_refined, int num_matches ){
+	//vector<DMatch> bestMatches = minHamming(matches); // no longer used
+
+	for (vector<DMatch>::iterator it = matches.begin(); it != matches.end(); ++it) {
 		//-- Get the keypoints from the good matches
 		kp1_refined.push_back(keypoints1[it->queryIdx].pt);
 		kp2_refined.push_back(keypoints2[it->trainIdx].pt);
 	}
-	matches = bestMatches;
 }
-/*
-void myRANSAC(Mat& H, vector<Point2f>& kp1_refined, vector<Point2f>& kp2_refined) {
-	int r, Ncounter = 0, Ocounter = 0;
-	float s_x, s_y, s_xn, s_yn, fin_x, fin_y;
-	vector<int> corr;
-	for (size_t k = 0; k < 50; k++)
-	{
-		r = rand() % kp1_refined.size;
-		s_x = kp2_refined.at(r).x - kp1_refined.at(r).x;
-		s_y = kp2_refined.at(r).y - kp1_refined.at(r).y;
-		for (size_t n = 0 ; n < kp1_refined.size(); ++n)
-		{
-			s_xn = kp2_refined.at(n).x - kp1_refined.at(n).x;
-			s_yn = kp2_refined.at(n).y - kp1_refined.at(n).y;
-			if (abs(s_xn - s_x) + abs(s_yn - s_y) < 5) {
-				Ncounter = Ncounter + 1;
-				corr[]
-			}
-		}
-		if (Ncounter > Ocounter) {
-			Ocounter = Ncounter;
-			fin_x = s_x;
-			fin_y = s_y;
 
-		}
-	}
-}*/
+/**
+ * @brief compute homography and select only the transaltion parameters
+ * an homography matrix which performs only translation looks like:
+ * [1 0 tx]
+ * [0 1 ty]
+ * [0 0 1]
+ * @param H output homography matrix
+ * @param kp1_refined keypoints for left image
+ * @param kp2_refined keypoints for right image
+ */
 void homogTranslation(Mat& H, vector<Point2f>& kp1_refined, vector<Point2f>& kp2_refined) {
 	H = findHomography(kp2_refined, kp1_refined, RANSAC,3);
+	cout << H << endl;
 	H.at<double>(0, 0) = 1;
 	H.at<double>(1, 0) = 0;
 	H.at<double>(0, 1) = 0;
@@ -198,9 +366,18 @@ void homogTranslation(Mat& H, vector<Point2f>& kp1_refined, vector<Point2f>& kp2
 	H.at<double>(2, 2) = 1;
 }
 
+/**
+ * @brief perform the alpha blend technique in order to have a gradual transaction
+ * from the left image to the right one.
+ * 
+ * @param img1 left image
+ * @param img2 right image
+ * @param mask mask with parameter for the alphablend transaction
+ * @param blended resulted blend image
+ */
 void alphaBlend(Mat& img1, Mat&img2, Mat& mask, Mat& blended) {
 	// Blend img1 and img2 (of CV_8UC3) with mask (CV_8UC1)
-	assert(img1.size() == img2.size() && img1.size() == mask.size());
+	assert(img1.size() == img2.size() && img1.size() == mask.size()); //sizes of img1, img2 and mask must coincide
 	blended = cv::Mat(img1.size(), img1.type());
 	for (int y = 0; y < blended.rows; ++y) {
 		for (int x = 0; x < blended.cols; ++x) {
@@ -210,11 +387,19 @@ void alphaBlend(Mat& img1, Mat&img2, Mat& mask, Mat& blended) {
 	}
 }
 
-void connectingImages(Mat& imgleft, Mat& imgright, Mat& H, Mat& result,int firstWidth) {
+/**
+ * @brief after computing the warping of the right image with respect to the left (using translation matrix)
+ * connects the panoramic image with the new right part, using the alphaBlend class
+ * 
+ * @param imgleft left image
+ * @param imgright right image
+ * @param H transaltion matrix
+ * @param result old panoramic image in input, new panoramic image in output
+ */
+void connectingImages(Mat& imgleft, Mat& imgright, Mat& H, Mat& result) {
 	Mat rightWarped;
 	warpPerspective(imgright, rightWarped, H, cv::Size(imgleft.cols + H.at<double>(0, 2), imgleft.rows));
-	
-
+	imwrite("rightwarped.png", rightWarped);
 	//resize right part
 	Mat biggerRight(result.rows, result.cols + H.at<double>(0, 2), CV_8UC3, Scalar(0));
 	Mat rightPart(biggerRight, Rect(biggerRight.cols - rightWarped.cols, 0, rightWarped.cols, rightWarped.rows));
@@ -239,54 +424,119 @@ void connectingImages(Mat& imgleft, Mat& imgright, Mat& H, Mat& result,int first
 			mask.col(c).setTo(0);
 		}
 	}
-	
+	//imwrite("mask.png", mask);
 	//alpha blending
 	alphaBlend(biggerLeft, biggerRight, mask, result);
 }
 
-void addRight(Mat& imgleft, Mat& imgright, Mat& result,int firstWidth,int i){
+/**
+ * @brief perform an iteration of the panoramic stitching process, adding an image to the right
+ * 
+ * @param imgleft the last image addedd to the panorama
+ * @param imgright the new image to add
+ * @param result the panorama so far, in the output will be the new panorama
+ * @param i number of the iteration, and the number of the image to add
+ * @param num_matches for testing purposes, set the number of features to detect in ORB
+ * @param yshift keep track of the shift on the y axis, will use for cut out the black borders
+ * @param descriptor used descriptor, ORB or AKAZE
+ */
+void addRight(Mat& imgleft, Mat& imgright, Mat& result,int i,int num_matches, double& yshift, int& descriptor, int& set){
 	//create mask for search keypoints in only half image
 	Mat maskRight, maskLeft;
 	std::cout << "creating Masks"<<endl;
 	maskImages(imgleft, imgright, maskRight, maskLeft);
 
-	//cylindrical projection
+
+	// TODO UNCOMMENT
+	
+	//cylindrical projection and equalization
 	std::cout << "performing cylindrical projection" << endl;
 	imgright = cylindricalProj(imgright, 33);
-
+	imgright = imageEqualization((void*)&imgright);
+	/*
+	string path = "pM/0"+to_string(i)+".jpg";
+	imwrite(path, imgright);*/
 
 	std::vector<KeyPoint> keypoints1, keypoints2;
 	Mat descriptors1, descriptors2;
-	std::cout << "running ORB detection"<<endl;
-	orbDetection(imgleft, maskRight, keypoints1, descriptors1);
-	orbDetection(imgright, maskLeft, keypoints2, descriptors2);
 
+	if (descriptor == 1) {
+		std::cout << "running ORB detection" << endl;
+
+		orbDetComp(imgleft, maskRight, keypoints1, descriptors1, num_matches,set,i);
+		orbDetComp(imgright, maskLeft, keypoints2, descriptors2, num_matches,set,i);
+	}
+	else if (descriptor == 2)
+	{
+		std::cout << "running AKAZE detection and computing" << endl;
+		akazeDetComp(imgleft, maskRight, keypoints1, descriptors1, num_matches, set);
+		akazeDetComp(imgright, maskLeft, keypoints2, descriptors2, num_matches, set);
+	}
 	vector<DMatch> matches;
 	std::cout << "computing matches" << endl;
 	matchingBF(descriptors1, descriptors2, matches);
 	std::vector<Point2f> kp1_refined;
 	std::vector<Point2f> kp2_refined;
 	std::cout << "matching refinement" << endl;
-	matchingRefine(matches, keypoints1, keypoints2, kp1_refined, kp2_refined);
+	matchingRefine(matches, keypoints1, keypoints2, kp1_refined, kp2_refined, num_matches);
 	
 	//Drawing matches
 	Mat comparison;
 	cv::drawMatches(imgleft, keypoints1, imgright, keypoints2, matches, comparison);
-	//string filename = "comparison " + to_string(i-1)+"-"+ to_string(i)+".png";
-	//imwrite(filename, comparison);
+	string filename = "comparison " + to_string(i-1)+"-"+ to_string(i)+".png";
+	imwrite(filename, comparison);
 	cv::imshow("comparison", comparison);
-	cv::waitKey(1000);
+	cv::waitKey(1);
 
 	//extract translation matrix from homography
 	Mat H;
 	std::cout << "estimating translation"<<endl;
 	homogTranslation(H, kp1_refined, kp2_refined);
+	//cout << "estimating homography"<<endl;
+	//H = findHomography(kp2_refined, kp1_refined, RANSAC, 3);
 	std::cout << H << endl;
 	std::cout << "connecting images"<<endl;
-	connectingImages(imgleft, imgright, H, result,firstWidth);
+	connectingImages(imgleft, imgright, H, result);
+	yshift = H.at<double>(1, 2);
+
 	
 	//cv::imshow("result", result);
 	//cv::waitKey();
 	//cv::destroyWindow(filename);
 	
 }
+
+/**
+ * @brief update the shift on the y axis during this iteration of addRight class
+ * 
+ * @param yshift the shift in this iteration
+ * @param oshiftPlus the maximum positive shift obtained in this imageset
+ * @param oshiftMinus the minimum negative shift obtained in this imageset
+ */
+void updateShift(double yshift, double& oshiftPlus, double& oshiftMinus) {
+	if (yshift > oshiftPlus) {
+		oshiftPlus = yshift;
+	}
+	else if (yshift < oshiftMinus)
+	{
+		oshiftMinus = yshift;
+	}
+}
+
+/**
+ * @brief cut out the black borders obtained due the shift on the y axis
+ * the final result will be writed as "cut.png"
+ * 
+ * @param result input image with black borders
+ * @param oshiftPlus the maximum positive shift obtained in this imageset
+ * @param oshiftMinus  the minimum negative shift obtained in this imageset
+ */
+void lastCut(Mat& result, double oshiftPlus, double oshiftMinus) {
+	cout << "Positive shift " << oshiftPlus << endl;
+	cout << "Negative shift" << oshiftMinus << endl;
+	//cv::imshow("result", result);
+	imwrite("resultBlended.png", result);
+	Mat cut(result, Rect(0, oshiftPlus, result.cols, result.rows + oshiftMinus - oshiftPlus));
+	imwrite("cut.png", cut);
+}
+
